c90686a047753c629d06a92748827eed903933a3
Kamente <justinm48000@gmail.com>
2023-09-03 22:44:32 +0300

    restaurant method instances

--- restaurant.py
+++ restaurant.py
@@ -12,3 +12,16 @@ class Restaurant(Base):
     rest_price = Column(Integer)
 
     reviews = relationship('Review', back_populates='restaurant')
+
+    def reviews(self):
+        return self.reviews  
+
+    def customers(self):
+        return [review.customer for review in self.reviews] 
+
+    @classmethod
+    def fanciest(cls):
+        return session.query(cls).order_by(cls.rest_price.desc()).first()  
+
+    def all_reviews(self):
+        return [review.full_review() for review in self.reviews] 
