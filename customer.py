46f9b2d0667feeb37c60d007b1f4741aa8ad1d39
Kamente <justinm48000@gmail.com>
2023-09-03 22:43:56 +0300

    defined customer method instances

--- customer.py
+++ customer.py
@@ -12,3 +12,27 @@ class Customer(Base):
     cust_lname = Column(String)
 
     reviews = relationship('Review', back_populates='customer')
+
+
+    def reviews(self):
+        return self.reviews  
+
+    def restaurants(self):
+        return [review.restaurant for review in self.reviews]  
+
+    def full_name(self):
+        return f"{self.cust_fname} {self.cust_lname}"  
+
+    def favorite_restaurant(self):
+        return max(self.reviews, key=lambda review: review.star_rating).restaurant 
+
+    def add_review(self, restaurant, rating):
+        new_review = Review(restaurant=restaurant, customer=self, star_rating=rating)
+        session.add(new_review)
+        session.commit()
+
+    def delete_reviews(self, restaurant):
+        reviews_to_delete = [review for review in self.reviews if review.restaurant == restaurant]
+        for review in reviews_to_delete:
+            session.delete(review)
+        session.commit()
