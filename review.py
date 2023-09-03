c3a9c083c60a215cad55661c2176a701c697ac6f
Kamente <justinm48000@gmail.com>
2023-09-03 22:45:07 +0300

    review method instances

--- review.py
+++ review.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, ForeignKey, Sequence
 from sqlalchemy.orm import relationship
-from restaurant import Restaurant
 
 Base = declarative_base()
 
@@ -14,3 +13,12 @@ class Review(Base):
     cust_id = Column(Integer, ForeignKey('customer.cust_id'))
     restaurant = relationship('Restaurant', back_populates='reviews')
     customer = relationship('Customer', back_populates='reviews')
+
+    def customer(self):
+        return self.customer 
+    
+    def restaurant(self):
+        return self.restaurant 
+
+    def full_review(self):
+        return f"Review for {self.restaurant.rest_name} by {self.customer.full_name()}: {self.star_rating} stars."
